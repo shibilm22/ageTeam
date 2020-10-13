b2bce1a6be6c0fd9cf5b586213f1688d97678780
shibilm22 <shibil.mandawe@patelhs.org>
2020-10-13 17:08:02 +0000

    added if else

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+   int age = 15;
+
+   if (age >= 15){
+     System.out.println("You are old enough to join the football team");
+
+  }else {
+
+     System.out.println("You are not old enough to join the football team");
+
+  for (int a = 15; a< 17; a++){
+
+  System.out.println(a + " ");
+     
+   }
+
   }
 }
+}
+
