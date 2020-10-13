acc029f043f66eb8d29e17d75277dead246010a7
shibilm22 <shibil.mandawe@patelhs.org>
2020-10-13 17:18:48 +0000

    added or statement

--- Main.java
+++ Main.java
@@ -4,17 +4,18 @@ class Main {
   public static void main(String[] args) {
    int age = 15;
 
-   if (age >= 15){
+   if (age >= 15 || age>=17){
      System.out.println("You are old enough to join the football team");
-
+    //you are old enough to join the football team if youre 15.
   }else {
 
      System.out.println("You are not old enough to join the football team");
-
+    //you cant join the footbal team because you are not old enough.
   for (int a = 15; a< 17; a++){
+  
 
   System.out.println(a + " ");
-  
+
      
    }
 
